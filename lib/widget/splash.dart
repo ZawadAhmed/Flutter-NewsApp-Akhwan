de4175726df2b0518abec9399d31a97055e0ccb0
ZawadAhmed <zawadwasik@gmail.com>
2023-01-31 20:40:09 +0800

    Main.dart update

---
+++ lib/widget/splash.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import '../view/homepage.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:shimmer/shimmer.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AnimatedSplashScreen(
+        splash: Shimmer.fromColors(
+          baseColor: Colors.white,
+          highlightColor: Color.fromARGB(255, 26, 37, 42),
+          child: Center(
+            child: Text(
+              "NewsPortalMY",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        nextScreen: Beranda(),
+        duration: 2000,
+        splashTransition: SplashTransition.fadeTransition,
+        pageTransitionType: PageTransitionType.fade,
+        backgroundColor: Colors.blue,
+      ),
+    );
+  }
+}
