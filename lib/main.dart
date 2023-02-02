0bebac58b9425dd8af2824bba54ebcbc1a38e9f6
ZawadAhmed <zawadwasik@gmail.com>
2023-02-02 18:18:22 +0800

    Testing Firebasse

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-//import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import './widget/splash.dart';
 
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    //Firebase.initializeApp();
+    Firebase.initializeApp();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'NewsPortalMy',
